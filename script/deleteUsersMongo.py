32b271aca834e0bbac0e4e3657fbfeb84225533f
Agustin <agusnavarro11@gmail.com>
2015-11-22 22:07:27 +0100

    Script databse

---
+++ script/deleteUsersMongo.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+from pymongo import MongoClient
+#Instalar pymongo: pip install pymongo
+
+client = MongoClient("mongodb://raven:raven@ds041144.mongolab.com:41144/raven")
+db = client.raven
+db.users.delete_many({});
+printf "Delete Users"
